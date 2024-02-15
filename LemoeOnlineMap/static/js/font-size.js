4eb1d142312953c2a09965f43d04f71e9b8df1d2
lyy0323 <lyy0323@sjtu.edu.cn>
2024-02-15 16:45:58 +0800

    Smaller font size.
    Signed-off-by: lyy0323 <lyy0323@sjtu.edu.cn>

--- LemoeOnlineMap/static/js/font-size.js
+++ LemoeOnlineMap/static/js/font-size.js
@@ -1,6 +1,6 @@
 
 var html = document.documentElement; //获取到html元素
 var hWidth = html.getBoundingClientRect().width;//获取到html的宽度
-if(hWidth > 640) hWidth = 500; // 当hWidth大于640时，则物理分辨率大于1280（这就看设备的devicePixelRatio这个值了），应该去访问pc网站了
-html.style.fontSize = hWidth/24 + "px"; //设置HTML的字体大小 font-size = 50px，1rem = 50px
+if(hWidth > 640) hWidth = 640; // 当hWidth大于640时，则物理分辨率大于1280（这就看设备的devicePixelRatio这个值了），应该去访问pc网站了
+html.style.fontSize = hWidth/32 + "px"; //设置HTML的字体大小 font-size = 50px，1rem = 50px
 
